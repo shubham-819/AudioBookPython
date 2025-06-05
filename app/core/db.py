f77e3e1216d5ac17fac79fe3d12114a16e82ab1d
Shubham Chaudhary <shubhamchaudhary819@gmail.com>
2025-06-05 23:43:19 +0530

    changed to accomodate the firebase creds

--- app/core/db.py
+++ app/core/db.py
@@ -1,6 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
+import os
 
-cred = credentials.Certificate("serviceAccountFirebase.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client() 
+# Fetch the secret from an environment variable
+firebase_creds = os.getenv("FIREBASE_CREDENTIALS")
+if not firebase_admin._apps:
+    cred = credentials.Certificate(json.loads(firebase_creds))
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
